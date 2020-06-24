91d8e4ed5b7b4c862f29a9bbe6145d3d9995de13
venkatesanhope <venkatesan.hope@gmail.com>
2020-06-24 19:38:45 +0530

    updated

--- src/app/pages/School/student/student/student.component.ts
+++ src/app/pages/School/student/student/student.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 //import {FormControl,FormGroup, FormArray} from '@angular/forms'
+
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-  //userForm:FormGroup;
+  
+  
 
   constructor() { 
-   // this.userForm = new FormGroup({
-     // 'application': new FormControl(),
-      //'Fname':new FormControl()
-    //})
+ 
+  }
+  
+ 
+  ngOnInit() {
+    //console.log(this.MyForm.form.valid);
   }
 
-  ngOnInit() {
-  }
 
 }
