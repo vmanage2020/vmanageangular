c91b6d278739361ade428ecf216daa7b468c82ad
venkatesanhope <venkatesan.hope@gmail.com>
2020-06-25 18:43:54 +0530

    updated

--- src/app/pages/School/student/student/student.component.ts
+++ src/app/pages/School/student/student/student.component.ts
@@ -12,6 +12,7 @@ export class StudentComponent implements OnInit {
   //userForm:FormGroup;
   breadCrumbItems: Array<{}>;
   validationform: FormGroup; // bootstrap validation form
+ // certificateInfo:FormArray;
 
   // Form submition
   submit: boolean;
@@ -22,20 +23,28 @@ export class StudentComponent implements OnInit {
   horizontalsubmit: boolean;
   constructor(public formBuilder: FormBuilder) { 
     this.validationform= new FormGroup({
+      'sName': new FormControl(),
+      'appno': new FormControl("5ef1d8778dd65"),
+      'dateapp': new FormControl("23-06-2020"),
       "firstName": new FormControl(),
       'middleName': new FormControl(),
+      'lastName' : new FormControl(),
+      //'entrym': new FormControl("Regular"),
+      'Batchyear': new FormControl(),
+      'boardname': new FormControl(),
+      
       'certificateInfo': new FormArray([
-        new FormGroup({
-          'certificateName':new FormControl(),
-          'certificateDate': new FormControl(),
-          'certificateNo' : new FormControl(),
-          'certificateAttach' : new FormControl()
-        })
-      ])
+       new FormGroup({
+         'certificateName':new FormControl(),
+         'certificateDate': new FormControl(),
+         'certificateNo' : new FormControl(),
+         'certificateAttach' : new FormControl()
+       })
+     ])
     })
   }
   
-  cinfo()
+ /* cinfo()
   {
     let vformarray = this.validationform.get('certificateInfo') as FormArray;
     vformarray.push(
@@ -45,13 +54,25 @@ export class StudentComponent implements OnInit {
       'certificateNo' : new FormControl(),
       'certificateAttach' : new FormControl()
     }))
-  }
+  }*/
   ngOnInit() {
     this.breadCrumbItems = [{ label: 'UBold', path: '/' }, { label: 'Forms', path: '/' }, { label: 'Form Validation', path: '/', active: true }];
  
     this.validationform = this.formBuilder.group({
+     sName: ['', [Validators.required]],
+     appno: ['5ef1d8778dd65', [Validators.pattern('[a-zA-Z0-9]+')]],
+      dateapp: ['23-06-2020', [Validators.pattern('[a-zA-Z0-9]+')]],
       firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
       middleName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+      lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]+')]],
+     // entrym: ['Regular', [Validators.required]],
+      Batchyear: ['', [Validators.required]],
+      boardname: ['', [Validators.required]],
+      certificateName:['', [Validators.required]],
+      certificateDate:['', [Validators.required]],
+      certificateNo:['', [Validators.required]],
+      certificateAttach:['', [Validators.required]]
+     
      
     });
 
