30ca4c63f898ec8570d2c91401fdc478ba194a01
venkatesanhope <venkatesan.hope@gmail.com>
2020-06-26 14:51:21 +0530

    update by venkat

--- src/app/pages/School/student/studentview/studentview.component.ts
+++ src/app/pages/School/student/studentview/studentview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-studentview',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentviewComponent implements OnInit {
 
-  constructor() { }
+  url='http://sms.akst.in/public/api/student/5';
+ mygrou=[1,2,3,4,5];
+  SViewitems=[];
+  
+  objectKeys = Object.keys;
+  constructor(private http:HttpClient) {
+    this.http.get<any>(this.url).toPromise().then(data => {
+   
+     for(let key in data)
+      if(data.hasOwnProperty(key))
+      this.SViewitems.push(data);
+      
+     // console.log( this.SListitems[1].users[0].stu_prf_stud_name+"_____________hi");
+    })
+   }
 
   ngOnInit() {
   }
--- src/app/pages/School/student/stuentlist/stuentlist.component.ts
+++ src/app/pages/School/student/stuentlist/stuentlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-stuentlist',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stuentlist.component.scss']
 })
 export class StuentlistComponent implements OnInit {
-
-  constructor() { }
+  url='http://sms.akst.in/public/api/students';
+ mygrou=[1,2,3,4,5];
+  SListitems=[];
+  contentlist=[];
+  objectKeys = Object.keys;
+  constructor(private http:HttpClient) {
+    this.http.get<any>(this.url).toPromise().then(data => {
+   
+     for(let key in data)
+      if(data.hasOwnProperty(key))
+      this.SListitems.push(data);
+      this.contentlist.push( this.SListitems[1]);
+     // console.log( this.SListitems[1].users[0].stu_prf_stud_name+"_____________hi");
+    })
+   }
 
   ngOnInit() {
   }
