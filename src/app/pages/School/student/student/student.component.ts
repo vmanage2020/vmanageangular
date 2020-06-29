95882bdcdca0999a10ac312d266ee46696569011
avimal <avimal@hlth360.net>
2020-06-29 21:43:49 +0530

    Updated

--- src/app/pages/School/student/student/student.component.ts
+++ src/app/pages/School/student/student/student.component.ts
@@ -10,6 +10,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class StudentComponent implements OnInit {
+
+  appRandomNumber:any;
   //userForm:FormGroup;
   breadCrumbItems: Array<{}>;
   validationform: FormGroup; // bootstrap validation form
@@ -65,7 +67,7 @@ export class StudentComponent implements OnInit {
      }))
    }*/
   ngOnInit() {
-
+    this.appRandomNumber = this.makeRandom();
 
     
 
@@ -356,6 +358,17 @@ export class StudentComponent implements OnInit {
      this.router.navigate(['/school/studentlist']);
    })
 
+  }
+
+  makeRandom()  {
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    const lengthOfCode = 10;  
+    let text = "";
+
+    for (let i = 0; i < lengthOfCode; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+      return text;
   }
 
 }
