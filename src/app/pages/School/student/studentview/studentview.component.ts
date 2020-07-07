d6f9b30846acb22e4d53b80d6497843e76553ce2
venkatesanhope <venkatesan.hope@gmail.com>
2020-07-07 10:02:53 +0530

    commited

--- src/app/pages/School/student/studentview/studentview.component.ts
+++ src/app/pages/School/student/studentview/studentview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-studentview',
   templateUrl: './studentview.component.html',
@@ -224,14 +224,14 @@ dropdownCertificateArray: any = [
     
   ]
 
-ctrlmainhead=["Account Information","Previous School Information","Parent Information","Student Information","Other Information","Permanent Address","Contact Address"]
+ctrlmainhead=["Account Information","Previous School Information","Parent Information","Student Information","Other Information","Foreigner Information","Permanent Address","Contact Address"]
 ctrlsubhead=[["School Name","Application No","Date Applied","Student First Name","Student Middle Name","Student Last Name","Entry Mode","Batch/Academic year","Board Name","Medium Name","Standard ","Group Name","Second Language","Reading ability","Mother Tongue","Date of Birth","Age"],
 ["Previous School Board Name","Medium Of Previous Study","Previous School Name","Previous Standard "],["Father/Guardian First Name","Father/Guardian Last Name","Father/Guardian Aadhar Number","Father Qualification","Father Occupation","Father Annual Income","Mother Name","Mother Occupation","Mother Annual Income","Parent Email ID","Parent Mobile No","Place of Living","Parents Old Student","Family size"],
-["Gender","Place of Birth","Blood Group","Religion","Community","Student Email ID","Student Aadhar Number","EMIS Number","Student Bank Details"],["Physically challenged","Visual Defect","Any Medical Issues","Co-Curriculum Activity","Language known","Citizen","Remarks","Student Image","Bus Facility Requirement"],["Address","State","Country","Pincode ","Phone Number","Nearest Railway Station"],
+["Gender","Place of Birth","Blood Group","Religion","Community","Student Email ID","Student Aadhar Number","EMIS Number","Student Bank Details"],["Physically challenged","Visual Defect","Any Medical Issues","Co-Curriculum Activity","Language known","Citizen","Remarks","Student Image","Bus Facility Requirement"],["Country","Passport No","Passport Validate Date","Visa No","Visa Expiry Date"],["Address","State","Country","Pincode ","Phone Number","Nearest Railway Station"],
 ["Address","State","Country","Pincode ","Phone Number"]]
 ctrllabel:any=[["col_code_fk","stu_prf_app_No","stu_prf_app_date","stu_prf_stud_name","stu_stud_mname","stu_stud_lname","stu_adm_mode","stu_prf_current_Year","stu_prf_degree_code","stu_prf_branch_Code","stu_prf_current_Semester","stu_prf_current_batch","stu_prf_sec_lang","stu_read_mode","stu_prf_mother_tongue_fk","stu_prf_dob","stu_prf_age"],["stu_prev_degree_code","stu_prev_medium_ins_fk","stu_adm_prev_colname","stu_adm_prev_class"],
 ["stu_prf_fathers_name","stu_prf_guardian_name","stu_prf_guardian_aadhar","stu_prf_fathers_qual","stu_prf_fathers_occup","stu_prf_fathers_anninc","stu_prf_mothers_name","stu_prf_mothers_occup","stu_prf_mothers_anninc","stu_prf_parent_email","stu_prf_mobile_no","stu_prf_plc_of_livng","stu_prf_parents_old_stu","stu_prf_family_size"],["stu_prf_sex","stu_prf_plc_of_birth","stu_prf_bldgrp_fk","stu_prf_religion_fk","stu_prf_community_fk","stu_prf_stu_email","stu_prf_stu_aadhar","stu_prf_stu_emis","stu_prf_stu_bank"],
-["stu_prf_parents_handicap","stu_prf_visualhandy","stu_prf_medical","stu_prf_co_curr","stu_prf_third_lang","stu_prf_citizen_fk","stu_prf_remarks","stu_adm_stu_image","stu_prf_bus"],["con_per_add","con_per_state","con_per_cntry","con_per_pincode","con_per_phone","con_rail_stn"],["con_per_add","con_per_state","con_per_cntry","con_per_pincode","con_per_phone"]]
+["stu_prf_parents_handicap","stu_prf_visualhandy","stu_prf_medical","stu_prf_co_curr","stu_prf_third_lang","stu_prf_citizen_fk","stu_prf_remarks","stu_adm_stu_image","stu_prf_bus"],["stu_foreign_country_name","stu_foreign_passport_no","stu_foreign_passport_valid_date","stu_foreign_visa_no","stu_foreign_visa_valid_date"],["con_per_add","con_per_state","con_per_cntry","con_per_pincode","con_per_phone","con_rail_stn"],["con_per_add","con_per_state","con_per_cntry","con_per_pincode","con_per_phone"]]
 checkdownarr=[[0,0,0,0,0,0,0,0,8,9,10,11,12,0,14,0,16],[0,1,25,3],[25,25,25,25,25,25,25,25,25,25,25,25,25,25],[25,25,2,3,4,25,25,25,25],[25,25,25,25,25,5,25,25,25],[25,25,25,25,25,25],[25,25,25,25,25],[]]
 dropdownArray:any = [[["", "Vidyalakshmi school", "Vidyalakshmi school2", "Vidyalakshmi school3", "Vidyalakshmi school4", "Vidyalakshmi school5"],[],[],[],[],[],[],["", "2020", "2021", "2019", "2023", "2024"],["", "METRIC ", "CBSE", "STATE", "ICSE", "OTHERS"],["", "English", "Tamil", "Sanskrit", "Hindi", "Telugu"],[ "","PRE-KG","LKG", "UKG", "I", "II","III","IV","V","VI","VII","VIII","IX","X", "XI", "XII"],["","GENERAL","SCIENCE","MATHS","SOCIAL","COMPUTER","AGRI"],["","English", "Tamil", "Sanskrit", "Hindi", "Telugu"],[],[ "","English", "Tamil", "Sanskrit", "Hindi", "Telugu"],[],[]],
 [[ "","metric ", "cbse", "state", "icse", "others10"],["", "English", "Tamil", "Sanskrit", "Hindi", "Telugu11"],[],  ["", "X", "XI", "XII", "XIII", "XIV12"]],[[],[],[],[],[],[],[],[],[],[],[],[],[],[]],[[],[],["", "A1positive", "A positive", "OPositive", "AB+", "AB-13"],["", "HINDU", "MUSLIM", "CHRISTIAN", "SIKKIM", "OTHERS14"],["", "BC", "MBC", "SC", "ST", "OTHERS15"],[],[],[],[]],[[],[],[],[], [ "","English", "Tamil", "Sanskrit", "Hindi", "Telugu","Kannada","Malayalam"],[ "","INDIAN", "FOREIGNER",  "OTHERS"],[],[],[]],[[],[],[],[],[],[]],[[],[],[],[],[]],[[],[],[],[]]]
@@ -246,6 +246,8 @@ sexval:any;
 phychan:any;
 visdef:any;
 busfacval:any;
+stuinf:any=25;
+stufore:any=25;
   constructor(private http:HttpClient,private route: ActivatedRoute) {
 
     let Metaurl='https://cors-anywhere.herokuapp.com/http://sms.akst.in/public/api/master';
@@ -344,6 +346,19 @@ busfacval:any;
         // console.log(this.SViewitems.student_documents);
         console.log(this.SViewDoc);
         this.parseDate(this.SViewitems.stu_prf_dob);
+        //console.log(this.SViewitems.stu_prf_current_Semester+"=="+"Pre-KG" + "||" +this.SViewitems.stu_prf_current_Semester+"=="+"LKG"+"__________________Hi");
+        if(this.SViewitems.stu_prf_current_Semester==1 || this.SViewitems.stu_prf_current_Semester==2)
+        {
+          this.stuinf=1;
+         $("#set_"+this.stuinf).hide();
+        // this.ctrlmainhead.splice(this.stuinf, 1);
+         
+        }
+        if(this.SViewitems.stu_prf_citizen_fk!=2)
+        {
+          this.stufore=5;
+          $("#set_"+this.stufore).hide();
+        }
         if(this.SViewitems.stu_adm_stu_image==null)
         {
           this.daimag="http://sms.akst.in/writable/uploads/student.png";
