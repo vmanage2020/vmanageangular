4d4b7b7bfc330255efa1f6e0205075833998aebe
swamykannan <swamykannanm@sciera.com>
2020-07-23 15:42:31 +0530

    changes added

--- src/app/pages/staff/staff.module.ts
+++ src/app/pages/staff/staff.module.ts
@@ -3,23 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { StaffRoutingModule } from './staff-routing.module';
 
-import { NgWizardModule, NgWizardConfig, THEME } from 'ng-wizard';
 
 import { StaffComponent } from './staff.component';
 import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
 import { ViewComponent } from './view/view.component';
 
-const ngWizardConfig: NgWizardConfig = {
-  theme: THEME.default
-};
 
 @NgModule({
   declarations: [StaffComponent, AddComponent, ListComponent, ViewComponent],
   imports: [
     CommonModule,
-    StaffRoutingModule,
-    NgWizardModule.forRoot(ngWizardConfig)
+    StaffRoutingModule
   ]
 })
 export class StaffModule { }
